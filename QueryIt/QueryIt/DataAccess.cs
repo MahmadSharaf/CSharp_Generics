a4bb891cb7e2bb3b40770bfab508ff10104ea10f
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-21 15:36:45 +0200

    Add Query It (Uncompleted)

---
+++ QueryIt/QueryIt/DataAccess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        T FindById(int id);
+        IQueryable<T> FindAll();
+        int Commit();
+    }
+
+    public class SqlRepository<T> : IRepository<T>
+    {
+        
+        public void Add(T newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
